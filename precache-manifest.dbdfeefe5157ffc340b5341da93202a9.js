deef707d334b393a38bc731add02606d34452078
Mykhalenych <mykhalenych@gmail.com>
2020-05-03 16:21:42 +0300

    Updates

---
+++ precache-manifest.dbdfeefe5157ffc340b5341da93202a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56001544fff527c636fcf5bd104cd321",
+    "url": "/airport-scoreboard/index.html"
+  },
+  {
+    "revision": "89b1bd2159d5e06eb239",
+    "url": "/airport-scoreboard/static/css/main.b45e2767.chunk.css"
+  },
+  {
+    "revision": "59a32887db9179356ecb",
+    "url": "/airport-scoreboard/static/js/2.7019d091.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/airport-scoreboard/static/js/2.7019d091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b1bd2159d5e06eb239",
+    "url": "/airport-scoreboard/static/js/main.0a50796a.chunk.js"
+  },
+  {
+    "revision": "c33dfe43b04e0ecb01d0",
+    "url": "/airport-scoreboard/static/js/runtime-main.037b15d8.js"
+  }
+]);
